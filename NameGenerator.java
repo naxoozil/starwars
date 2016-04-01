70440109b07857683da422995e085afb4a0b6f94
naxoozil <nachoviuda@gmail.com>
2016-04-01 13:08:13 +0200

    Actualizado para quitar los literales

--- NameGenerator.java
+++ NameGenerator.java
@@ -7,14 +7,21 @@
  */
 public class NameGenerator
 {
+    public static final int VALOR_INICIAL = 0;
+    public static final int VALOR_FINAL_TU_PRIMER_APELLIDO =3;
+    public static final int VALOR_FINAL_NOMBRE =3;
+    public static final int VALOR_FINAL_PRIMER_APELLIDO_MADRE =2;
+    public static final int VALOR_FINAL_NOMBRE_CIUDAD =3;
+    
     /**
      * Metodo para crear un nombre de Star Wars a partir de lo pedido
      * 
      */
     public void generateStarWarsName(String tuPrimerApellido, String nombre, String primerApellidoMadre, String nombreCiudadNacimiento)
     {
-        String nombreStarWars = tuPrimerApellido.substring(0,3) + nombre.substring(0,3);
-        String apellidoStarWars = primerApellidoMadre.substring(0,2) + nombreCiudadNacimiento.substring(0,3);
+        
+        String nombreStarWars = tuPrimerApellido.substring(NameGenerator.VALOR_INICIAL,NameGenerator.VALOR_FINAL_TU_PRIMER_APELLIDO) + nombre.substring(NameGenerator.VALOR_INICIAL,NameGenerator.VALOR_FINAL_NOMBRE);
+        String apellidoStarWars = primerApellidoMadre.substring(NameGenerator.VALOR_INICIAL,NameGenerator.VALOR_FINAL_PRIMER_APELLIDO_MADRE) + nombreCiudadNacimiento.substring(NameGenerator.VALOR_INICIAL,NameGenerator.VALOR_FINAL_NOMBRE_CIUDAD);
         System.out.println(nombreStarWars + " " + apellidoStarWars);
     }
 }
